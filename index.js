49eae8315b7b39a277cbe12fa3a88e81df554b70
liohumb <helloliohumb@gmail.com>
2023-03-01 11:52:32 +0100

    configuration & setup backend api

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
 
+/* CONFIGURATIONS */
+const __filename = fileURLToPath( import.meta.url )
+const __dirname = path.dirname( __filename )
+dotenv.config()
 const app = express()
 
-app.use(cors())
-app.use(express.json())
-app.listen(8080, () => {
-    console.log('Le serveur est en route => PORT: 8080')
-})
+app.use( express.json() )
+app.use( helmet() )
+app.use( helmet.crossOriginResourcePolicy( { policy: 'cross-origin' } ) )
+app.use( morgan( 'common' ) )
+app.use( bodyParser.json( { limit: '30mb', extended: true } ) )
+app.use( bodyParser.urlencoded( { limit: '30mb', extended: true } ) )
+app.use( cors() )
+app.use( '/assets', express.static( path.join( __dirname, 'public/assets' ) ) )
 
-mongoose.set('strictQuery', true)
-mongoose.connect('mongodb://localhost:27017/entre2mains').then(console.log('Connecté à MongoDb avec succès !'))
+/* FILE STORAGE */
+const storage = multer.diskStorage( {
+    destination: ( req, file, callback ) => {
+        callback( null, 'public/assets' )
+    },
+    filename: ( req, file, callback ) => {
+        callback( null, file.originalname )
+    }
+} )
+const upload = multer( { storage } )
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001
+
+mongoose.set( 'strictQuery', true )
+mongoose.connect( process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then( () => {
+    app.listen(PORT, (  ) => console.log( `Le serveur est en route => PORT: ${PORT}` ))
+}).catch( ( error ) => console.log(`${error} connexion impossible`))
